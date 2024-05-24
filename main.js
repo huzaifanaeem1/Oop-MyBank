4e0ab557b2bbdb9d7b1d4380474722546b9c44e0
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-05-25 02:54:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+// Oop MyBank Project
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Bank account class
+class BankAccount {
+    accountNumber;
+    balance;
+    constructor(accountNumber, balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    // debit money
+    withdrawl(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            console.log(chalk.green(`Withdrawal of $${amount} successful. Remaining balance: $${this.balance}`));
+        }
+        else {
+            console.log(chalk.red("Insufficient balance"));
+        }
+    }
+    // credit money
+    deposit(amount) {
+        if (amount > 100) {
+            amount -= 1; // $1 fee charged if more than $100 is deposited
+        }
+        this.balance += amount;
+        console.log(chalk.green(`Deposit of $${amount} successful. Remaining balance: $${this.balance}`));
+    }
+    // check balance
+    checkBalance() {
+        console.log(chalk.blue(`Current balance: $${this.balance}`));
+    }
+}
+// Customer class
+class Customer {
+    firstName;
+    lastName;
+    gender;
+    age;
+    mobileNumber;
+    account;
+    constructor(firstName, lastName, gender, age, mobileNumber, account) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.account = account;
+    }
+}
+// Create bank accounts
+const accounts = [
+    new BankAccount(1001, 1000),
+    new BankAccount(1002, 2000),
+    new BankAccount(1003, 3000)
+];
+// Create customers
+const customers = [
+    new Customer("Huzaifa", "Naeem", "Male", 17, 3351848651, accounts[0]),
+    new Customer("Haseeb", "Sadiq", "Male", 18, 3331118866, accounts[1]),
+    new Customer("Aliyan", "Saleem", "Male", 18, 3335558866, accounts[2])
+];
+// Function to interact with bank account
+async function service() {
+    do {
+        const accountNumberInput = await inquirer.prompt({
+            name: "accountNumber",
+            type: "number",
+            message: "Enter your account number:"
+        });
+        const customer = customers.find(customer => customer.account.accountNumber === accountNumberInput.accountNumber);
+        if (customer) {
+            console.log(chalk.yellow(`Welcome, ${customer.firstName} ${customer.lastName}!\n`));
+            const result = await inquirer.prompt([
+                {
+                    name: "select",
+                    type: "list",
+                    message: "Select an operation",
+                    choices: [
+                        "Deposit", "Withdrawl", "Check Balance", "Exit"
+                    ]
+                }
+            ]);
+            switch (result.select) {
+                case "Deposit":
+                    const depositAmount = await inquirer.prompt([{
+                            name: "amount",
+                            type: "number",
+                            message: "Enter the amount to deposit:"
+                        }]);
+                    customer.account.deposit(depositAmount.amount);
+                    break;
+                case "Withdrawl":
+                    const withdrawlAmount = await inquirer.prompt([{
+                            name: "amount",
+                            type: "number",
+                            message: "Enter the amount to withdraw:"
+                        }]);
+                    customer.account.withdrawl(withdrawlAmount.amount);
+                    break;
+                case "Check Balance":
+                    customer.account.checkBalance();
+                    break;
+                case "Exit":
+                    console.log(chalk.magenta("Exiting Bank Program..."));
+                    console.log(chalk.cyan("\nThank you for using our bank services"));
+                    return;
+            }
+        }
+        else {
+            console.log(chalk.red("Invalid account number. Please try again."));
+        }
+    } while (true);
+}
+service();
